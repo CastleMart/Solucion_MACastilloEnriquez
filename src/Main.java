52045122dcdd3c7f94e5fa3435a7e7edd30432b4
MarioCastillo <marioenriquez650@gmail.com>
2022-11-15 21:30:00 -0500

    Verificación del funcionamiento del método Movimiento.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Movimiento mov = new Movimiento("Retiro", 400, "Para compar una coca");
+        int fecha [] = mov.getFechaMovimiento();
+
+
+
+        System.out.printf("Fecha tomada fue: %d/ %d/ %d", fecha[0], fecha[1], fecha[2]);
+
+        System.out.println("tipoMov: " + mov.getTipoMovimiento());
+        System.out.println("Monto:$" + mov.getMonto());
     }
 }
--- src/Movimiento.java
+++ src/Movimiento.java
@@ -1,16 +1,38 @@
+import java.util.Calendar;
+
 public class Movimiento {
     //Atributos de la clase Movimiento.
-    private  int fechaMovimiento;
+    private int fechaMovimiento [] = new int[3];
     private String tipoMovimiento;
     private double monto;
     private String concepto;
 
-    
+
     public Movimiento(String tipoMovimiento, double monto, String concepto){
 
+        Calendar fecha = Calendar.getInstance();
 
+        this.fechaMovimiento[0] = fecha.get(Calendar.DATE);
+        this.fechaMovimiento[1] = fecha.get(Calendar.MONTH);
+        this.fechaMovimiento[2] = fecha.get(Calendar.YEAR);
         this.tipoMovimiento = tipoMovimiento;
         this.monto = monto;
         this.concepto = concepto;
+    }
+
+    public int[] getFechaMovimiento() {
+        return fechaMovimiento;
+    }
+
+    public String getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
     }
 }
